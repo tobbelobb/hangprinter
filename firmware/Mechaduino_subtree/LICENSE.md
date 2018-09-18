cb1dc6012505823429b05448764724dc91094ec1
tobbelobb <tobben@fastmail.fm>
2018-09-18 10:25:25 +0200

    Removes Mechaduino firmware

--- firmware/Mechaduino_subtree/LICENSE.md
+++
@@ -1,5 +0,0 @@
-All Mechaduino related materials are released under the
-
-Creative Commons Attribution Share-Alike 4.0 License
-
-https://creativecommons.org/licenses/by-sa/4.0/
--- firmware/Mechaduino_subtree/Mechaduino/Mechaduino/Controller.h
+++
@@ -1,6 +0,0 @@
-// TC5 Handler declaration
-#ifndef __CONTROLLER_H__
-#define  __CONTROLLER_H__
-#define WAIT_TC16_REGS_SYNC(x) while(x->COUNT16.STATUS.bit.SYNCBUSY);
-void TC5_Handler();
-#endif
