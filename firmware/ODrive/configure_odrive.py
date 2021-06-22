31b3c0e7dab4a4921e2d91ede61dd229c50b8a50
Torbjørn Ludvigsen <tobben@fastmail.fm>
2021-06-22 13:54:19 +0200

    Updates odrive config instructions

--- firmware/ODrive/configure_odrive.py
+++ firmware/ODrive/configure_odrive.py
@@ -7,6 +7,9 @@ odrv0 = odrive.find_any()
 # Default values used on the dev setup.
 # If your setup is different in any way, you might have to change these values
 #odrv0.config.brake_resistance = 0.4699999988079071 # Default
+# Later ODrive Firmware versions (0.5.2 onwards) might disable break resistor by default
+# So you need to set a boolean to true to enable it again:
+# odrv0.config.enable_brake_resistor = true;
 
 #odrv0.axis0.motor.config.pole_pairs = 7            # Default
 #odrv0.axis0.controller.current_setpoint = 0        # Default. Torque mode with zero torque
@@ -77,17 +80,24 @@ odrv0.axis1.controller.config.vel_gain = 0.09
 
 # Interface
 odrv0.config.enable_uart = False
+# Later ODrive Firmware versions (0.5.2 onwards) require you to disable the default uart like this instead:
+# odrv0.config.enable_uart_a = False
 
 #odrv0.can.set_baud_rate(250000) # Default
-# odrv0.axis0.config.can_node_id = 0 # Default
-# odrv0.axis1.config.can_node_id = 1 # Default
+# odrv0.axis0.config.can_node_id = 50
+# odrv0.axis1.config.can_node_id = 52
 # For the other board
- odrv0.axis0.config.can_node_id = 2
- odrv0.axis1.config.can_node_id = 3
+odrv0.axis0.config.can_node_id = 51
+odrv0.axis1.config.can_node_id = 53
+# Later ODrive Firmware versions (0.5.2 onwards) configure can_node_id like this:
+# odrv0.axis0.config.can.node_id = 51
 
 odrv0.axis0.config.step_gpio_pin = 1
 odrv0.axis0.config.dir_gpio_pin = 2
 odrv0.axis0.config.turns_per_step = 0.0025 # 1/(25*16) = 0.0025
+# Later ODrive Firmware versions (0.5.2 onwards) configure turns_per_step like this:
+# odrv0.axis0.axis.controller.config.steps_per_circular_range = 400 # 25*16
+
 odrv0.axis0.config.enable_step_dir = True
 
 #odrv0.axis1.config.step_gpio_pin = 7 # Default
